437d91dc4ae17792b7bc9d3d9359664b3288a84d
Jimmy Breck-McKye <jbreckmckye@users.noreply.github.com>
2016-02-27 00:46:37 +0000

    Update bundle for better CommonJS support

--- trkl.js
+++ trkl.js
@@ -1,9 +1,16 @@
-(function(root, factory) {
-    if (typeof define === "function" && define.amd) {
-        define("trkl", [], factory);
-    } else {
-        root.trkl = factory();
-    }
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define([], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory(require('b'));
+	} else {
+		// Browser globals (root is window)
+		root.trkl = factory();
+	}
 }(this, function() {
     var computedTracker = [];
 
