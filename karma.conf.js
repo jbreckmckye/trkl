e4e08c9ef910f367342cfbe2f3a41625e8ff2d2e
Jimmy Breck-McKye <jbreckmckye@users.noreply.github.com>
2017-05-18 10:58:57 +0100

    Even smaller

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'trkl.js',
+      'trkl.js', // use trkl-min.js to test minified version
       'spec.js'
     ],
 
