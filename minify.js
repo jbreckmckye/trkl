496a9d7a613842072351c6ef45014f69c50f2e6b
jbreckmckye <jbreck-mckye@virtuocityvr.com>
2017-06-01 12:55:23 +0100

    New minification script

--- minify.js
+++ minify.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const gzipSize = require('gzip-size');
 const UglifyJS = require('uglify-es');
 
 const source = fs.readFileSync('trkl.js', 'utf8');
@@ -20,3 +21,8 @@ const options = {
 const result = UglifyJS.minify(source, options);
 
 fs.writeFileSync('trkl-min.js', result.code);
+
+const initSize = source.length;
+const minSize = result.code.length;
+const zipSize = gzipSize.sync(result.code);
+console.log(`File size: compressed from ${initSize} characters to ${minSize} characters. Predicted GZIP size is ${zipSize}`);
