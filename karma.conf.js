d0579bca91ae610e79cc990cedb6316d623e4333
Jimmy Breck-McKye <3148617+jbreckmckye@users.noreply.github.com>
2022-06-26 13:48:12 +0100

    1. Migrate to Jest 2. Optimise 393 -> 383 bytes 3. Update documentation

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'trkl-min.js', // use trkl-min.js to test minified version
-      'spec.js'
+      'test.js'
     ],
 
 
